9b71314bbab073de4d2178955c30cb8d57740f6f
Clément Gayot <contact@clement-gayot.fr>
2025-12-22 19:28:06 +0100

    temp 2 : fix load icons on folder change

--- src/src/MessagesService.js
+++ src/src/MessagesService.js
@@ -15,6 +15,9 @@ class MessagesService {
      * Timestamp of the last display inbox list call.
      */
     this.lastDisplayInboxListCall = 0;
+    this.isPending = false;
+    this.pendingTab = null;
+    this.pendingTriggeredFromDOMEvent = false;
   }
 
   /**
@@ -318,10 +321,17 @@ class MessagesService {
    */
   async displayInboxList(tab, triggeredFromDOMEvent = false) {
     if (!this.canDisplayInboxList()) {
-      if (triggeredFromDOMEvent) {
-        const remainingTime = this.WAIT_TIME_MS - (Date.now() - this.lastDisplayInboxListCall);
-        setTimeout(() => this.displayInboxList(tab, triggeredFromDOMEvent), remainingTime + 50);
+      this.pendingTab = tab;
+      this.pendingTriggeredFromDOMEvent = triggeredFromDOMEvent;
+      if (this.isPending) {
+        return;
       }
+      this.isPending = true;
+      const remainingTime = this.WAIT_TIME_MS - (Date.now() - this.lastDisplayInboxListCall);
+      setTimeout(() => {
+        this.isPending = false;
+        this.displayInboxList(this.pendingTab, this.pendingTriggeredFromDOMEvent);
+      }, remainingTime + 50);
       return;
     }
     this.updateLastDisplayInboxListCall();
